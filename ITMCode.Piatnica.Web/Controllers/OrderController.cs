622bd6d11432c5e394a44a072736d49911a31989
michalmazur3 <michalmazur3@gmail.com>
2019-07-15 13:21:07 +0200

    starting web project

---
+++ ITMCode.Piatnica.Web/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITMCode.Piatnica.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult OrderList()
+        {
+            return View("OrderList");
+        }
+        public IActionResult AddOrder()
+        {
+            return View();
+        }
+
+    }
+}
