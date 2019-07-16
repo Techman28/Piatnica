6f17fcce21c36e3b52d2699e5cea5ae315bfad29
michalmazur3 <michalmazur3@gmail.com>
2019-07-16 11:26:28 +0200

    no message

--- ITMCode.Api/Migrations/20190716080052_eventName.Designer.cs
+++ ITMCode.Api/Migrations/20190716080052_eventName.Designer.cs
@@ -4,14 +4,16 @@ using ITMCode.Piatnica.Dal;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ITMCode.Piatnica.Api.Migrations
 {
     [DbContext(typeof(PiatnicaContext))]
-    partial class PiatnicaContextModelSnapshot : ModelSnapshot
+    [Migration("20190716080052_eventName")]
+    partial class eventName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ITMCode.Api/Migrations/20190716080052_eventName.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ITMCode.Piatnica.Api.Migrations
+{
+    public partial class eventName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "EventsHistoriesContext",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "EventsHistoriesContext");
+        }
+    }
+}
