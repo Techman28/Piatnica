152c0d1facd7eb9a945f2f3e01506b28a979c8dc
Techman28 <techman2809@gmail.com>
2019-07-17 15:41:25 +0200

    no message

---
+++ ITMCode.Piatnica.Web/Controllers/EmployeesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ITMCode.Piatnica.Web.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public EmployeesController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("PiatnicaApi");
+        }
+
+        public IActionResult EmployeeList()
+        {
+            return View("EmployeesList");
+        }
+
+
+
+    }
+}
---
+++ ITMCode.Piatnica.Web/wwwroot/js/vechicleList.js
@@ -0,0 +1,67 @@
+﻿$(document).ready(function () {
+
+     
+
+
+    var dataSet = [
+        {  "1", "Aliquam", "2093", "448065", "24" },
+        {  "2",  "sit",  "6751",  "335185" ,"20"},
+        {  "3",  "nunc",  "9350",  "352941", "20"},
+
+    ];
+
+    var table = $('#datatables').DataTable({
+        data: dataSet,
+    columns: [
+        {
+            "orderable": false,
+            "searchable": false,
+            "title": ""
+        },
+        {
+            "orderable": false,
+            "searchable": true,
+            "title": "Marka"
+        },
+        {
+            "orderable": false,
+            "searchable": true,
+            "title": "Nr Rejestracyjny"
+        },
+        {
+            "orderable": false,
+            "searchable": true,
+            "title": "Przebieg"
+        },
+        {
+            "orderable": false,
+            "searchable": true,
+            "title": "Ładowność"
+        },
+
+    ]
+});
+
+
+
+
+
+// Edit record
+table.on('click', '.edit', function () {
+    $tr = $(this).closest('tr');
+    var data = table.row($tr).data();
+    alert('You press on Row: ' + data[0] + ' ' + data[1] + ' ' + data[2] + '\'s row.');
+});
+
+// Delete a record
+table.on('click', '.remove', function (e) {
+    $tr = $(this).closest('tr');
+    table.row($tr).remove().draw();
+    e.preventDefault();
+});
+
+//Like record
+table.on('click', '.like', function () {
+    alert('You clicked on Like button');
+});
+});
